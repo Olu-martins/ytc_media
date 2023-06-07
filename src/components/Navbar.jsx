3eef17ff07d258011306f5690427dc409ec55f2a
olu-martins <mayowasamuel86@gmail.com>
2023-06-07 03:29:30 +0100

    add search to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Stack } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { logo } from '../utils/constant';
+import SearchBar from './SearchBar';
 
 const Navbar = () => (
   <Stack
@@ -12,6 +13,7 @@ const Navbar = () => (
     <Link style={{display:'flex', alignItems:'center'}}>
       <img src={logo} alt="logo" height={45} />
     </Link>
+    <SearchBar />
   </Stack>
   )
 
